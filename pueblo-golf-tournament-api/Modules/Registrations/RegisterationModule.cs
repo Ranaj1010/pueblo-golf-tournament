7357b1f1edf89ddfa71df77040824669eba1856e
Eve Michael Jayson Selmo <jayson.selmo@rafi.ph>
2023-07-09 19:57:29 +0800

    fixed(code): member from previos input cleared and duplicate entry of Team.

--- pueblo-golf-tournament-api/Modules/Registrations/RegisterationModule.cs
+++ pueblo-golf-tournament-api/Modules/Registrations/RegisterationModule.cs
@@ -397,7 +397,6 @@ namespace pueblo_golf_tournament_api.Modules.Registrations
             var response = new RegisteredPlayerDto();
             try
             {
-
                 var existingPlayer = await _playerService.GetAsync(player => player.PersonId == payload.PersonId);
 
                 if (existingPlayer != null)
@@ -496,7 +495,9 @@ namespace pueblo_golf_tournament_api.Modules.Registrations
                     return response;
                 }
 
-                var registeredTeam = await _teamService.AddAsync(new Team
+                var existingTeamName = await _teamService.GetAsync(team => team.Name.Equals(payload.Name));
+
+                var registeredTeam = existingTeamName != null ? existingTeamName :  await _teamService.AddAsync(new Team
                 {
                     Name = payload.Name,
                     TeamCaptainId = payload.TeamCaptainId,
